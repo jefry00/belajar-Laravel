3c84bea098f09afcac8fdd9b24952b614f6b13ca
jefry00 <jepripasaribu98@gmail.com>
2021-10-13 20:03:46 +0700

    first commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-medann                                                          tfdiyfitd                                                                                                                                                                                                                                                                                                   
+                                                                                                                                                                                                                                                                                                
         <title>Test page</title>
 
         <!-- Fonts -->
@@ -24,8 +24,9 @@ medann                                                          tfdiyfitd
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
             @if (Route::has('login'))
                 <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
-                    @auth
-                        <a href="{{ url('/home') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Home</a>
+                 
+                @auth
+                         <a href="{{ url('/home') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Home</a>
                     @else
                         <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
 
